62443eb68127f77fe7e5115c339261b86e54fa3c
emmilyyy111 <86429560+emmilyyy111@users.noreply.github.com>
2021-07-12 10:49:54 -0400

    Add files via upload

---
+++ cars.js
@@ -0,0 +1,18 @@
+const cars = [{
+    make: 'Audi',
+    model: 'A3',
+    year: 2018
+}, {
+    make: 'Mazda',
+    model: 'Miata',
+    year: 1992
+}]
+
+exports.getCars = (request, response) => {
+    response.send(cars)
+}
+
+exports.getSingleCar = (request, response) => {
+    const { carId } = request.params
+    response.send(cars[carId])
+}
